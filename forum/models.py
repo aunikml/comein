c34ba25b1a2a3618090d5ac595f87a909a51e5f0
Bracied <you@example.com>
2025-04-15 14:39:31 +0600

    tweak_forum&profile

--- forum/models.py
+++ forum/models.py
@@ -28,7 +28,7 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     content = HTMLField()
     # content = models.TextField()
-    context = models.CharField(max_length=20, choices=CONTEXT_CHOICES)
+    context = models.CharField(max_length=20, choices=CONTEXT_CHOICES, blank=True, null=True)
 
     def __str__(self):
         return self.title
